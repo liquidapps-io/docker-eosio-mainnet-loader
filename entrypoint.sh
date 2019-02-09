dc410ae74776b5b484b82adb0082e0328225d7eb
Tal Muskal <tal@bancor.network>
2019-02-09 15:56:40 +0000

    using s3

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
+cd /eos.data
+# rm -rf blocks/blocks.index  blocks/reversible state snapshot
 
-# todo, skip if exists
-mkdir /eos.data.staging
-cd /eos.data.staging
-rm -rf blocks/blocks.index  blocks/reversible state snapshot
 
-url=`wget -q 'https://eosnode.tools/api/snapshots?limit=24' -O - | jq -e '.data[].s3' -r | grep 07-00.tar | head -n 1`
-wget "$url" -O - | tar xzf -
-
-if [ ! -f /eos.data.staging/blocks/blocks.log ]; then
-        if [ ! -f /eos.data.staging/blocks_backup.tar.gz ]; then
-                wget $(wget --quiet "https://eosnode.tools/api/blocks?limit=1" -O- | jq -r '.data[0].s3') -O blocks_backup.tar.gz
+# skip if exists
+if [ ! -f /eos.data/blocks/blocks.log ]; then
+        aws sts get-caller-identity && export HAVE_S3="true"
+        if [ "$HAVE_S3" == "true" ]
+        then
+                aws s3 cp s3://liquideos.mainnet.backup/latest/snapshot.tar.gz snapshot.tar.gz --request-payer requester
+                aws s3 cp s3://liquideos.mainnet.backup/latest/blocks_backup.tar.gz blocks_backup.tar.gz --request-payer requester
+        else
+                url=`wget -q 'https://eosnode.tools/api/snapshots?limit=24' -O - | jq -e '.data[].s3' -r | grep 07-00.tar | head -n 1`
+                wget "$url" -O snapshot.tar.gz
+                if [ ! -f /eos.data/blocks_backup.tar.gz ]; then
+                        wget $(wget --quiet "https://eosnode.tools/api/blocks?limit=1" -O- | jq -r '.data[0].s3') -O blocks_backup.tar.gz
+                fi
         fi
         tar xvzf blocks_backup.tar.gz
+        tar xvzf snapshot.tar.gz
+        rm /eos.data/snapshot.tar.gz
+        rm /eos.data/blocks_backup.tar.gz
 fi
-
-set -x
-set -e
-
-rm /eos.data.staging/blocks_backup.tar.gz
-cp -a /eos.data.staging /eos.data
-#rm /eos.data.staging/* -rf
