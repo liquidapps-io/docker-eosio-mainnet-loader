1597b4643d2e0cdaba0a3120596385eaaf5e3f23
Tal Muskal <tal@bancor.network>
2019-02-12 21:52:45 +0000

    added: clear db flag

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-url=`wget -q 'https://eosnode.tools/api/snapshots?limit=24' -O - | jq -e '.data[].s3' -r | grep 07-00.tar | head -n 1`
+url=`wget -q 'https://eosnode.tools/api/snapshots?limit=24' -O - | jq -e '.data[].s3' -r | grep 06-00.tar | head -n 1`
 wget "$url" -O snapshot.tar.gz
 
 wget $(wget --quiet "https://eosnode.tools/api/blocks?limit=1" -O- | jq -r '.data[0].s3') -O blocks_backup.tar.gz
